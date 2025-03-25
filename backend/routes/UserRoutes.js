e7a11b7069d0fcd41b76ced15e2daed8b6417821
Leonardo de Paula <leodepaula315@gmail.com>
2025-03-25 20:09:02 -0300

    Refatorar rotas de usuário para adicionar validação de dados

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 // controller
 const { register } = require("../controllers/UserController");
 
+// middlewares
+const validate = require("../middlewares/handlerValidation")
+
 // Routes
-router.post("/register", register);
+router.post("/register", validate, register);
 
 module.exports = router;
