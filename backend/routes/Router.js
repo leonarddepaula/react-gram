b9eba4d2b8fff04d78cf3d5b4db057d2086f9cb3
Leonardo de Paula <leodepaula315@gmail.com>
2025-03-25 19:09:14 -0300

    Add UserRoutes to Router.js

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,6 +1,9 @@
  const express = require("express")
  const router = express()
 
+ // User route
+ router.use("/api/users", require("./UserRoutes"))
+
  //test route
  router.get("/", (req, res) => {
     res.send("API Working!")
